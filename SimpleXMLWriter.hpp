e920d92ff40ac3443b22aec19d880b2ef858e766
sneakyevil <sneakyevildogs@gmail.com>
2023-02-14 13:37:14 +0100

    First commit

---
+++ SimpleXMLWriter.hpp
@@ -0,0 +1,104 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <map>
+
+class CXMLWriterNode
+{
+public:
+    std::string Key;
+    std::string Value;
+    int Depth = 0;
+
+    std::map<std::string, CXMLWriterNode> Nodes;
+    std::map<std::string, std::string> Attributes;
+
+    CXMLWriterNode& operator=(const char* m_Value)
+    {
+        Value = m_Value;
+        return *this;
+    }
+
+    CXMLWriterNode& operator[](const char* m_Key)
+    {
+        CXMLWriterNode& m_Node = Nodes[m_Key];
+        m_Node.Key      = m_Key;
+        m_Node.Depth    = Depth + 1;
+
+        return m_Node;
+    }
+
+    CXMLWriterNode& operator()(const char* m_AttributeKey, const char* m_AttributeValue)
+    {
+        Attributes[m_AttributeKey] = m_AttributeValue;
+        return *this;
+    }
+
+    std::string GetOpenTag()
+    {
+        std::string m_String = "<" + Key;
+
+        for (auto& m_Attribute : Attributes)
+            m_String += " " + m_Attribute.first + "=\"" + m_Attribute.second + "\"";
+
+        return (m_String + ">");
+    }
+
+    std::string GetCloseTag() { return ("</" + Key + ">");  }
+};
+
+class CXMLWriter
+{
+public:
+    CXMLWriterNode m_Node;
+    CXMLWriterNode& operator[](const char* m_Key) { return m_Node[m_Key]; }
+
+    bool m_InsertHeader = true;
+    std::string m_Version   = "1.0";
+    std::string m_Encoding  = "UTF-8";
+    std::string GetHeader() { return "<?xml version=\"" + m_Version + "\" encoding=\"" + m_Encoding + "\"?>\n"; }
+
+    void BuildString(std::string& m_String, CXMLWriterNode& m_Node)
+    {
+        for (auto& m_Nodes : m_Node.Nodes)
+        {
+            CXMLWriterNode& m_NextNode = m_Nodes.second;
+
+            std::string m_Tabs(m_NextNode.Depth - 1, '\t');
+
+            if (m_NextNode.Nodes.empty())
+                m_String += m_Tabs + m_NextNode.GetOpenTag() + m_NextNode.Value + m_NextNode.GetCloseTag() +"\n";
+            else
+            {
+                m_String += m_Tabs + m_NextNode.GetOpenTag() + "\n";
+                if (!m_NextNode.Value.empty())
+                    m_String += m_Tabs + "\t" + m_NextNode.Value + "\n";
+
+                BuildString(m_String, m_NextNode);
+                m_String += m_Tabs + m_NextNode.GetCloseTag() + "\n";
+            }
+        }
+    }
+
+    operator std::string()
+    {
+        std::string m_String = (m_InsertHeader ? GetHeader() : "");
+        BuildString(m_String, m_Node);
+
+        return m_String;
+    }
+
+    std::string GetString() { return operator std::string(); }
+
+    bool OutputFile(const char* m_Path)
+    {
+        FILE* m_File = fopen(m_Path, "w");
+        if (!m_File)
+            return false;
+
+        std::string m_String = GetString();
+        fwrite(&m_String[0], sizeof(char), m_String.size(), m_File);
+        fclose(m_File);
+
+        return true;
+    }
+};
